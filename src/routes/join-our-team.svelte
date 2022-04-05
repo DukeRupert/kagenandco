1985bdb371ac85b43717660254345e36bfd418f0
Logan Williams <logan@firefly.llc>
2022-04-04 18:02:46 -0700

    Fixed layout issues with join-our-team page

--- src/routes/join-our-team.svelte
+++ src/routes/join-our-team.svelte
@@ -96,251 +96,312 @@
 />
 <!-- Contact section -->
 <section class="relative bg-white" aria-labelledby="join-our-team">
-	<div class="absolute w-full h-1/2 bg-white" aria-hidden="true" />
 	<div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-		<div class="relative bg-white mb-8">
-			<h2 id="join-our-team" class="sr-only">Join Our Team</h2>
-
-			<div class="grid grid-cols-1 lg:grid-cols-3">
-				<!-- Contact information -->
+		<div class="flex flex-col lg:flex-row">
+			<div class="flex-1 flex flex-col">
 				<div class="relative overflow-hidden py-10 px-6 bg-secondary sm:px-10 xl:p-12">
 					<img
 						src="/kccLogoNew.svg"
 						alt="Kagen Coffee and Crepes Rooster logo"
-						class="w-full h-auto"
+						class="w-full max-w-2xl h-auto m-auto"
 					/>
 				</div>
-				<!-- Contact form -->
-				<div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-					<h3 class="text-lg font-medium text-gray-900">Join Our Team!</h3>
-					<!-- svelte-ignore component-name-lowercase -->
-					<form use:form class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-						<div>
-							<label for="first-name" class="block text-sm font-medium text-gray-900"
-								>First name</label
-							>
-							<div class="mt-1">
-								<input
-									type="text"
-									id="first-name"
-									name="firstName"
-									autocomplete="given-name"
-									class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-								/>
-								<ValidationMessage for="firstName" let:messages>
-									{messages || ''}
-								</ValidationMessage>
-							</div>
-						</div>
-						<div>
-							<label for="last-name" class="block text-sm font-medium text-gray-900"
-								>Last name</label
-							>
-							<div class="mt-1">
-								<input
-									type="text"
-									id="last-name"
-									name="lastName"
-									autocomplete="family-name"
-									class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-								/>
-								<ValidationMessage for="lastName" let:messages>
-									{messages || ''}
-								</ValidationMessage>
-							</div>
-						</div>
-						<div class="hidden">
-							<label for="password" class="block text-sm font-medium text-gray-900">Password</label>
-							<div class="mt-1">
-								<input
-									type="text"
-									id="password"
-									name="password"
-									class="block py-3 px-4 w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-								/>
-							</div>
-						</div>
-						<div>
-							<label for="email" class="block text-sm font-medium text-gray-900">Email</label>
-							<div class="mt-1">
-								<input
-									id="email"
-									type="email"
-									name="email"
-									autocomplete="email"
-									class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-								/>
-								<ValidationMessage for="email" let:messages>
-									{messages || ''}
-								</ValidationMessage>
-							</div>
-						</div>
-						<div>
-							<div class="flex justify-between">
-								<label for="tel" class="block text-sm font-medium text-gray-900">Phone</label>
-								<span id="tel-optional" class="text-sm text-gray-500">Optional</span>
-							</div>
-							<div class="mt-1">
-								<input
-									type="text"
-									id="tel"
-									name="tel"
-									autocomplete="tel"
-									class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-									aria-describedby="tel-optional"
-								/>
-								<ValidationMessage for="tel" let:messages>
-									{messages || ''}
-								</ValidationMessage>
-							</div>
-						</div>
-						<div>
-							<label for="location" class="block text-sm font-medium text-gray-900">Location</label>
-							<select
-								id="location"
-								name="location"
-								class="mt-1 block w-full pl-3 pr-10 py-3 px-4 text-base border-gray-300 focus:outline-none focus:ring-matisse-500 focus:border-matisse-500 sm:text-sm rounded-md"
-							>
-								<option selected>Tri-Cities</option>
-								<option>Spokane</option>
-							</select>
-						</div>
-						<div class="sm:col-span-2">
-							<!-- Legal to work? -->
-							<label>
-								<input
-									type="checkbox"
-									name="legal"
-									value="Yes"
-									class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
-								/>
-								Are you legally eligible to work in the US?
-							</label>
-						</div>
-						<div class="sm:col-span-2">
-							<!-- Veteran? -->
-							<label>
-								<input
-									type="checkbox"
-									name="veteran"
-									value="Yes"
-									class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
-								/>
-								Are you a Veteran?
-							</label>
-						</div>
-						<div class="sm:col-span-2">
-							<!-- Background Check? -->
-							<label>
-								<input
-									type="checkbox"
-									name="backgroundCheck"
-									value="Yes"
-									class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
-								/>
-								If selected for employment are you willing to submit to a background check?
-							</label>
-						</div>
-						<div class="sm:col-span-2">
-							<!-- Start Date? -->
-							<label>
-								Available start date:
-								<input type="date" name="startDate" required />
-							</label>
-						</div>
-						<div class="sm:col-span-2">
-							<!-- Full-time, Part-time or Seasonal/Temporary? -->
-							<fieldset>
-								<legend>Type of position desired?</legend>
-								<p>
-									<label>
-										<input
-											class="focus:ring-custard-500 text-custard-500"
-											type="radio"
-											name="employmentType"
-											value="full-time"
-										/> Full-time
-									</label>
-								</p>
-								<p>
-									<label>
-										<input
-											class="focus:ring-custard-500 text-custard-500"
-											type="radio"
-											name="employmentType"
-											value="part-time"
-										/> Part-time
-									</label>
-								</p>
-								<p>
-									<label>
-										<input
-											class="focus:ring-custard-500 text-custard-500"
-											type="radio"
-											name="employmentType"
-											value="seasonal"
-										/> Seasonal/Temporary
-									</label>
-								</p>
-							</fieldset>
-						</div>
-						<div class="col-span-2">
-							<p>Work History</p>
-						</div>
-						{#each Array(count) as _, i}
-							<div>
-								<label for="jobs[{i}][title]" class="block text-sm font-medium text-gray-900"
-									>Job Title</label
-								>
-								<div class="mt-1">
-									<input
-										type="text"
-										id="jobs[{i}][title]"
-										name="jobs[{i}][title]"
-										class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-									/>
-								</div>
-							</div>
-							<div>
-								<label for="jobs[{i}][years]" class="block text-sm font-medium text-gray-900"
-									>How Long?</label
-								>
-								<div class="mt-1 flex">
-									<input
-										type="number"
-										placeholder="years"
-										id="jobs[{i}][years]"
-										name="jobs[{i}][years]"
-										class="py-3 px-2 flex-initial w-24 shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-									/>
+			</div>
+			<div class="flex-1 flex flex-col">
+				<div class="relative bg-white mb-8">
+					<h2 id="join-our-team" class="sr-only">Join Our Team</h2>
 
-									<input
-										type="number"
-										placeholder="months"
-										id="jobs[{i}][months]"
-										name="jobs[{i}][months]"
-										class="py-3 px-2 ml-4 flex-inital w-24 shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
-									/>
+					<form use:form class="space-y-8 divide-y divide-gray-200">
+						<div class="space-y-8 divide-y divide-gray-200">
+							<div class="pt-8">
+								<div>
+									<h3 class="text-lg leading-6 font-medium text-gray-900">Join Our Team</h3>
+									<!-- <p class="mt-1 text-sm text-gray-500">
+								Use a permanent address where you can receive mail.
+							</p> -->
+								</div>
+								<div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+									<div class="sm:col-span-3">
+										<label for="firstName" class="block text-sm font-medium text-gray-700">
+											First name
+										</label>
+										<div class="mt-1">
+											<input
+												type="text"
+												name="firstName"
+												id="firstName"
+												autocomplete="given-name"
+												class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+											/>
+											<ValidationMessage for="firstName" let:messages>
+												{messages || ''}
+											</ValidationMessage>
+										</div>
+									</div>
+
+									<div class="sm:col-span-3">
+										<label for="lastName" class="block text-sm font-medium text-gray-700">
+											Last name
+										</label>
+										<div class="mt-1">
+											<input
+												type="text"
+												name="lastName"
+												id="lastName"
+												autocomplete="family-name"
+												class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+											/>
+											<ValidationMessage for="lastName" let:messages>
+												{messages || ''}
+											</ValidationMessage>
+										</div>
+									</div>
+
+									<div class="sm:col-span-3">
+										<label for="email" class="block text-sm font-medium text-gray-700">
+											Email address
+										</label>
+										<div class="mt-1">
+											<input
+												id="email"
+												name="email"
+												type="email"
+												autocomplete="email"
+												class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+											/>
+											<ValidationMessage for="email" let:messages>
+												{messages || ''}
+											</ValidationMessage>
+										</div>
+									</div>
+
+									<div class="sm:col-span-3">
+										<label for="tel" class="block text-sm font-medium text-gray-700"> Phone </label>
+
+										<div class="mt-1">
+											<input
+												type="text"
+												name="tel"
+												id="tel"
+												autocomplete="tel"
+												class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+											/>
+											<ValidationMessage for="tel" let:messages>
+												{messages || ''}
+											</ValidationMessage>
+										</div>
+									</div>
+
+									<div class="sm:col-span-3">
+										<label for="location" class="block text-sm font-medium text-gray-700">
+											Location
+										</label>
+										<div class="mt-1">
+											<select
+												id="location"
+												name="location"
+												class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+											>
+												<option selected>Tri-Cities</option>
+												<option>Spokane</option>
+											</select>
+										</div>
+									</div>
 								</div>
 							</div>
-						{/each}
-						<div>
-							<button
-								on:click={() => (count = count + 1)}
-								class="mt-2 px-4 py-3 shadow-sm text-gray-900 border border-gray-300 rounded-md {count >
-								4
-									? 'hidden'
-									: 'visible'}">+</button
-							>
+							<div class="mt-6">
+								<fieldset>
+									<!-- <legend class="text-base font-medium text-gray-900"> By Email </legend> -->
+									<div class="mt-4 space-y-4">
+										<div class="relative flex items-start">
+											<div class="flex items-center h-5">
+												<input
+													type="checkbox"
+													name="legal"
+													value="Yes"
+													class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
+												/>
+											</div>
+											<div class="ml-3 text-sm">
+												<label for="legal" class="font-medium text-gray-700"
+													>Are you legally eligible to work in the US?</label
+												>
+											</div>
+										</div>
+										<div class="relative flex items-start">
+											<div class="flex items-center h-5">
+												<input
+													type="checkbox"
+													name="veteran"
+													value="Yes"
+													class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
+												/>
+											</div>
+											<div class="ml-3 text-sm">
+												<label for="veteran" class="font-medium text-gray-700"
+													>Are you a veteran?</label
+												>
+											</div>
+										</div>
+										<div class="relative flex items-start">
+											<div class="flex items-center h-5">
+												<input
+													type="checkbox"
+													name="backgroundCheck"
+													value="Yes"
+													class="focus:ring-custard-500 h-4 w-4 text-custard-600 border-gray-300 rounded"
+												/>
+											</div>
+											<div class="ml-3 text-sm">
+												<label for="backgroundCheck" class="font-medium text-gray-700"
+													>Are you willing to submit to a background check?</label
+												>
+											</div>
+										</div>
+									</div>
+								</fieldset>
+								<fieldset class="mt-6">
+									<div>
+										<legend class="text-base font-medium text-gray-900"> Start Date </legend>
+										<p class="text-sm text-gray-500">What date would you be available to start?</p>
+									</div>
+									<div class="mt-4 space-y-4">
+										<div class="relative flex items-start">
+											<div class="flex items-center h-5">
+												<input
+													type="date"
+													name="startDate"
+													class="border-gray-300 rounded"
+													required
+												/>
+											</div>
+											<div class="ml-3 text-sm" />
+										</div>
+									</div>
+								</fieldset>
+								<fieldset class="mt-6">
+									<div>
+										<legend class="text-base font-medium text-gray-900"> Employment Type </legend>
+										<p class="text-sm text-gray-500">What kind of job are you looking for?</p>
+									</div>
+									<div class="mt-4 space-y-4">
+										<div class="flex items-center">
+											<input
+												id="full-time"
+												class="focus:ring-custard-500 text-custard-500"
+												type="radio"
+												name="employmentType"
+												value="full-time"
+											/>
+											<label for="full-time" class="ml-3 block text-sm font-medium text-gray-700">
+												Full-Time
+											</label>
+										</div>
+										<div class="flex items-center">
+											<input
+												id="part-time"
+												class="focus:ring-custard-500 text-custard-500"
+												type="radio"
+												name="employmentType"
+												value="part-time"
+											/>
+											<label for="part-time" class="ml-3 block text-sm font-medium text-gray-700">
+												Part-time
+											</label>
+										</div>
+										<div class="flex items-center">
+											<input
+												id="seasonal"
+												class="focus:ring-custard-500 text-custard-500"
+												type="radio"
+												name="employmentType"
+												value="seasonal"
+											/>
+											<label for="seasonal" class="ml-3 block text-sm font-medium text-gray-700">
+												Seasonal
+											</label>
+										</div>
+									</div>
+								</fieldset>
+							</div>
+							<div class="mt-6">
+								<fieldset class="mt-6">
+									<div>
+										<legend class="text-base font-medium text-gray-900"> Work History </legend>
+										<p class="text-sm text-gray-500">Please list your prior work experience.</p>
+									</div>
+									{#each Array(count) as _, i}
+										<div class="mt-4 space-y-4">
+											<div class="relative flex items-start">
+												<div>
+													<label
+														for="jobs[{i}][title]"
+														class="block text-sm font-medium text-gray-900">Job Title</label
+													>
+													<div class="mt-1">
+														<input
+															type="text"
+															id="jobs[{i}][title]"
+															name="jobs[{i}][title]"
+															class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
+														/>
+													</div>
+												</div>
+												<div class="mx-4">
+													<label
+														for="jobs[{i}][years]"
+														class="block text-sm font-medium text-gray-900">How Long?</label
+													>
+													<div class="mt-1 flex">
+														<input
+															type="number"
+															placeholder="years"
+															id="jobs[{i}][years]"
+															name="jobs[{i}][years]"
+															class="py-3 px-2 flex-initial w-24 shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
+														/>
+
+														<input
+															type="number"
+															placeholder="months"
+															id="jobs[{i}][months]"
+															name="jobs[{i}][months]"
+															class="py-3 px-2 ml-4 flex-inital w-24 shadow-sm text-gray-900 focus:ring-highlight focus:border-highlight border-gray-300 rounded-md"
+														/>
+													</div>
+												</div>
+
+												<!-- <div class="flex items-center h-5">
+										<input type="date" name="startDate" class="border-gray-300 rounded" required />
+									</div> -->
+											</div>
+										</div>
+									{/each}
+									<div class="mt-4 space-y-4">
+										<div>
+											<button
+												on:click={() => (count = count + 1)}
+												class="mt-2 px-4 py-3 shadow-sm text-gray-900 border border-gray-300 rounded-md {count >
+												4
+													? 'hidden'
+													: 'visible'}">+</button
+											>
+										</div>
+									</div>
+								</fieldset>
+							</div>
 						</div>
-						<div class="sm:col-span-2 sm:flex sm:justify-end">
-							<button
-								type="submit"
-								value="Submit Form"
-								class="mt-2 w-full flex items-center justify-center px-6 py-3 rounded-3xl border border-transparent shadow-sm bg-custard-500 text-base font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
-							>
-								Submit
-							</button>
+
+						<div class="pt-5">
+							<div class="flex items-center justify-center">
+								<button
+									type="submit"
+									value="Submit Form"
+									class="w-full max-w-xs flex items-center justify-center px-6 py-3 rounded-3xl border border-transparent shadow-sm bg-custard-500 text-base text-center font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
+								>
+									Submit
+								</button>
+							</div>
 						</div>
 					</form>
 				</div>
