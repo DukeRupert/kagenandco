b9bbbdf894f10270bd4ffcfbdbca19bd9add1581
Logan Williams <logan@firefly.llc>
2022-06-16 09:26:48 -0600

    testing env variables on vercel deployment

--- src/routes/api/utils/postToShopify.js
+++ src/routes/api/utils/postToShopify.js
@@ -1,13 +1,16 @@
 export const postToShopify = async ({ query, variables }) => {
 	try {
-		const result = await fetch(process.env.SHOPIFY_API_ENDPOINT, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_API_TOKEN
-			},
-			body: JSON.stringify({ query, variables })
-		}).then((res) => res.json());
+		const result = await fetch(
+			'https://kagens-coffee-and-crepes.myshopify.com/api/2022-04/graphql.json',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_API_TOKEN
+				},
+				body: JSON.stringify({ query, variables })
+			}
+		).then((res) => res.json());
 		if (result.errors) {
 			console.log({ errors: result.errors });
 		} else if (!result || !result.data) {
