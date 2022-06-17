34c96e795387ad176248d396aff1bd9992112d3b
Logan Williams <logan@firefly.llc>
2022-06-17 10:41:55 -0600

    Styled Counter

--- src/lib/components/Counter.svelte
+++ src/lib/components/Counter.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div
-	class="basis-1/4 border rounded-md mt-3 flex items-center justify-center text-base font-medium text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full"
+	class="basis-1/4 border rounded-md mt-3 py-3 flex items-center justify-center text-base font-medium text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full"
 >
 	<button on:click|preventDefault={decrementQuantity} class="basis-1/4 h-full"> - </button>
 	<p class="basis-1/2 h-full flex justify-center items-center text-center">{quantity}</p>
