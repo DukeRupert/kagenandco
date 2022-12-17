01dd5d8a562e311b2aa47f34268dd4de754265fc
Logan Williams <logan@firefly.llc>
2022-12-17 13:42:22 -0700

    Updated TextSection import

--- src/lib/components/TextSection.svelte
+++ src/lib/components/TextSection.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import PortableText from '@portabletext/svelte';
+	import { PortableText } from '@portabletext/svelte';
 	import Link from './Link.svelte';
 	export let portableText;
 	const { name, body } = portableText.block;
 </script>
 
-<PortableText blocks={body} serializers={{ marks: { link: Link } }} />
+<PortableText value={body} components={{ marks: { link: Link } }} />
