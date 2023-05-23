b100bc12cbb75cbddd50a21b032b83d984e9aae9
Logan <logan@firefly.llc>
2023-05-22 19:17:17 -0600

    Added animation to mobile nav menu

--- src/lib/components/header/Nav.svelte
+++ src/lib/components/header/Nav.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { isCartOpen } from '$lib/stores';
-	import { fade } from 'svelte/transition';
-	import { quadIn, quadOut, quartOut } from 'svelte/easing';
+	import { fly } from 'svelte/transition';
+	import { quadOut } from 'svelte/easing';
 	import { itemCount } from '$lib/stores';
 	import Logo from '$lib/components/Logo.svelte';
 	export let count: number = 0;
@@ -124,52 +123,61 @@
 	</div>
 </nav>
 <!-- Mobile menu, show/hide based on menu open state. -->
-<div class="{is_mobile_open ? 'block' : 'hidden'} lg:hidden" role="dialog" aria-modal="true">
-	<!-- Background backdrop, show/hide based on slide-over state. -->
-	<div class="fixed inset-0 z-50" />
+{#if is_mobile_open}
 	<div
-		class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+		transition:fly={{ duration: 300, x: 400, easing: quadOut }}
+		class="fixed inset-0 {is_mobile_open
+			? 'translate-x-0'
+			: 'translate-x-full'} z-50 min-h-screen lg:hidden"
+		role="dialog"
+		aria-modal="true"
 	>
-		<div class="flex items-center justify-between">
-			<a href="/" class="-m-1.5 p-1.5">
-				<span class="sr-only">Kagen's Coffee and Crepes</span>
-				<div class="h-12 w-12">
-					<Logo stroke="#000" fill="#000" />
-				</div>
-			</a>
-			<button
-				type="button"
-				on:click={toggle_mobile_open}
-				class="-m-2.5 rounded-md p-2.5 text-gray-700"
-			>
-				<span class="sr-only">Close menu</span>
-				<svg
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					aria-hidden="true"
+		<!-- Background backdrop, show/hide based on slide-over state. -->
+		<div class="fixed inset-0 z-50" />
+		<div
+			class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+		>
+			<div class="flex items-center justify-between">
+				<a href="/" class="-m-1.5 p-1.5">
+					<span class="sr-only">Kagen's Coffee and Crepes</span>
+					<div class="h-12 w-12">
+						<Logo stroke="#000" fill="#000" />
+					</div>
+				</a>
+				<button
+					type="button"
+					on:click={toggle_mobile_open}
+					class="-m-2.5 rounded-md p-2.5 text-gray-700"
 				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-				</svg>
-			</button>
-		</div>
-		<div class="mt-6 flow-root">
-			<div class="-my-6 divide-y divide-gray-500/10">
-				<div class="space-y-2 py-6">
-					{#each top_links as { title, href }}
-						<a
-							{href}
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 !no-underline !text-token"
-							>{title}</a
-						>
-					{/each}
-				</div>
-				<div class="py-6">
-					<a href="#" class="btn variant-filled-primary">Order Online</a>
+					<span class="sr-only">Close menu</span>
+					<svg
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="mt-6 flow-root">
+				<div class="-my-6 divide-y divide-gray-500/10">
+					<div class="space-y-2 py-6">
+						{#each top_links as { title, href }}
+							<a
+								{href}
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 !no-underline !text-token"
+								>{title}</a
+							>
+						{/each}
+					</div>
+					<div class="py-6">
+						<a href="#" class="btn variant-filled-primary">Order Online</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/if}
