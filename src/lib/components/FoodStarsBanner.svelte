1ca95a7db36f3bed47657dcc32f2011c196c9a20
Logan <logan@firefly.llc>
2023-05-23 20:13:12 -0600

    Resized GR banner image. Added height and width attributes.

--- src/lib/components/FoodStarsBanner.svelte
+++ src/lib/components/FoodStarsBanner.svelte
@@ -1,7 +1,9 @@
-<div class="relative">
+<div class="relative bg-black">
 	<img
 		src="\images\food_stars_gordan_ramsay_banner.jpg"
 		alt="Gordan Ramsay Food Stars series banner"
-		class="h-full w-full object-contain"
+		class="mx-auto h-full w-full max-w-7xl object-contain"
+		width="1600"
+		height="900"
 	/>
 </div>
