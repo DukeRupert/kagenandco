da182f5ebbcf1bf916bce9f07920bb912ee9113e
Logan <logan@firefly.llc>
2023-05-25 09:28:17 -0600

    UI: LocationCard buttons updated

--- src/lib/components/LocationCard.svelte
+++ src/lib/components/LocationCard.svelte
@@ -38,36 +38,18 @@
 	</div>
 	<div class="flex-1">
 		<div class="mt-4 sm:mt-6">
-			<a
-				href={orderUrl}
-				class="inline-flex justify-center w-full rounded-3xl border border-transparent shadow-sm px-4 py-2 lg:py-4 bg-custard-500 text-base font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
-			>
-				Order Online
-			</a>
+			<a href={orderUrl} class="btn variant-filled-primary min-w-[150px]"> Order Online </a>
 		</div>
 		<div class="mt-4 sm:mt-6">
-			<a
-				href={`/menu/${slug.current}`}
-				class="inline-flex justify-center w-full rounded-3xl border border-custard-500 shadow-sm px-4 py-2 lg:py-4 bg-custard-white text-base font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
-			>
+			<a href={`/menu/${slug.current}`} class="btn variant-ringed-primary min-w-[150px]">
 				View Menu
 			</a>
 		</div>
 		<div class="mt-4 sm:mt-6">
-			<a
-				href={locationUrl}
-				class="inline-flex justify-center w-full rounded-3xl border border-custard-500 shadow-sm px-4 py-2 lg:py-4 bg-custard-white text-base font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
-			>
-				Get Directions
-			</a>
+			<a href={locationUrl} class="btn variant-ringed-primary min-w-[150px]"> Get Directions </a>
 		</div>
 		<div class="mt-4 sm:mt-6">
-			<a
-				href={`phone: ${phone}`}
-				class="inline-flex justify-center w-full rounded-3xl border border-custard-500 shadow-sm px-4 py-2 lg:py-4 bg-custard-white text-base font-medium text-black hover:bg-custard-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custard-300 sm:text-sm"
-			>
-				Call Us
-			</a>
+			<a href={`phone: ${phone}`} class="btn variant-ringed-primary min-w-[150px]"> Call Us </a>
 		</div>
 	</div>
 </div>
