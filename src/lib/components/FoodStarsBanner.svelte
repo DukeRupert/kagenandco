18b07094d1f81f10602d79c4e7036973635f39e8
Logan <logan@firefly.llc>
2023-07-03 08:13:37 -0600

    Implemented svelte-img package on Gordan Ramsey banner component.

--- src/lib/components/FoodStarsBanner.svelte
+++ src/lib/components/FoodStarsBanner.svelte
@@ -1,9 +1,13 @@
+<script>
+	import gr_food_stars_banner from '$lib/assets/food_stars_gordan_ramsay_banner.jpg?run';
+	import { FxReveal as Img } from '@zerodevx/svelte-img';
+</script>
+
 <div class="relative bg-black">
-	<img
-		src="\images\food_stars_gordan_ramsay_banner.jpg"
+	<Img
+		src={gr_food_stars_banner}
 		alt="Gordan Ramsay Food Stars series banner"
 		class="mx-auto h-full w-full max-w-7xl object-contain"
-		width="1600"
-		height="900"
+		loading="eager"
 	/>
 </div>
