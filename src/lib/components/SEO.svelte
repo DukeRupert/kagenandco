ef8c7fc2b82dbceef8b19d92adea08ac343fcaa9
Logan <logan@firefly.llc>
2023-07-04 08:21:21 -0600

    Improved SEO

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  interface SeoData {
+    title: string;
+    description: string;
+    url: string;
+    og: {
+      src: string;
+      alt: string;
+      mimeType: string;
+      width: number;
+      height: number;
+    };
+  }
+
+  export let data: SeoData;
+</script>
+
+<svelte:head>
+  <title>{data.title}</title>
+  <meta name="description" content={data.description} />
+  <meta property="og:title" content={data.title} />
+  <meta property="og:description" content={data.description} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={data.url} />
+  <meta property="og:image:url" content={data.og.src} />
+  <meta property="og:image:alt" content={data.og.alt} />
+  <meta property="og:image:type" content={data.og.mimeType} />
+  <meta property="og:image:width" content={data.og.width.toString()} />
+  <meta property="og:image:height" content={data.og.height.toString()} />
+  <link rel="canonical" href={data.url} />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
+	import SEO from './../lib/components/SEO.svelte';
 	import Instagram from './Instagram.svelte';
 	import CTA from '$lib/components/CTA.svelte';
-	import Reviews from '$lib/components/reviews/Reviews.svelte';
 	import type { PageData, ActionData } from './$types';
-	import SvelteSeo from 'svelte-seo';
-	import { page } from '$app/stores';
-	import { urlFor } from '$lib/image-url';
 	import Hero from '$lib/components/hero/ImageTiles.svelte';
 	import ContactUs from '$lib/forms/ContactUs.svelte';
 	import FoodStarsBanner from '$lib/components/FoodStarsBanner.svelte';
@@ -14,35 +11,61 @@
 	// Sanity Content
 	export let data: PageData;
 	export let form: ActionData;
-	$: ({ title, description, mainImage } = data);
+
+	const seoData = {
+		title: 'Kagen Coffee & Crepes - Coffee, Crepes, and Community!',
+		description:
+			'Families and friends need a place to gather, catch up and simply enjoy life together. Our delicious crepes and crafted coffees are here to help you deepen friendships and build memories today.',
+		url: 'https://www.kagenandco.com/',
+		og: {
+			src: 'https://www.kagenandco.com/images/kagen_coffee_and_crepes_og.jpg',
+			alt: 'Kagen Coffee and Crepes landing page',
+			mimeType: 'image/jpg',
+			width: 1200,
+			height: 630
+		}
+	};
 </script>
 
-<SvelteSeo
-	{title}
-	{description}
-	canonical={$page.url.toString()}
-	openGraph={{
-		title: title,
-		description: description,
-		url: $page.url.toString(),
-		type: 'website',
-		images: [
-			{
-				url: urlFor(mainImage.asset).width(600).height(600).format('webp').url(),
-				width: '600',
-				height: '600',
-				alt: mainImage.alt
-			}
-		]
-	}}
-/>
+<SEO data={seoData} />
+<svelte:head>
+	<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Restaurant",
+  "name": "Kagen Coffee & Crepes",
+  "image": [
+    "https://www.kagenandco.com/_app/immutable/assets/black_bean_crepe_kagen_coffee_and_crepes.6c87b078.jpg",
+    "https://www.kagenandco.com/_app/immutable/assets/oreo_crepe_smiling_employee_kagen_coffee_and_crepes.fc0343bf.jpg",
+    "https://www.kagenandco.com/_app/immutable/assets/mixed_berry_crepe_kagen_coffee_and_crepes.9bab80ab.jpg",
+    "https://www.kagenandco.com/_app/immutable/assets/the_chunky_monkey_crepe_kagen_coffee_and_crepes.c6a037e6.jpg"
+  ],
+  "telephone": "+1509-851-7186",
+  "email": "info@kagenandco.com",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "270 Williams Blvd",
+    "addressLocality": "Richland",
+    "addressRegion": "WA",
+    "postalCode": "99354"
+  },
+  "menu": "https://www.kagenandco.com/menu/tri-cities",
+  "servesCuisine": "Coffee shop",
+  "url": "https://www.kagenandco.com/",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.6",
+    "ratingCount": "609"
+  }
+}
+	</script>
+</svelte:head>
 
 <FoodStarsBanner />
 <Hero />
 <QuoteOverlap />
 <Instagram />
 <TestimonialGrid />
-<!-- <Reviews /> -->
 <CTA />
 <div id="contact-us" class="relative isolate bg-white pb-32 pt-24 sm:pt-32">
 	<div class="relative mx-auto max-w-4xl mb-8">
