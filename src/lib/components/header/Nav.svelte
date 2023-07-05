39237b18d8e14b6cf5e78c3ab1182e33e497f32f
Logan <logan@firefly.llc>
2023-07-04 18:47:57 -0600

    Updated Nav to close when clicking outside of the menu

--- src/lib/components/header/Nav.svelte
+++ src/lib/components/header/Nav.svelte
@@ -144,6 +144,8 @@
 		<!-- Background backdrop, show/hide based on slide-over state. -->
 		<div
 			transition:fade={{ delay: 250, duration: 100, easing: quadOut }}
+			on:click={() => (is_mobile_open = false)}
+			on:keydown|stopPropagation={() => (is_mobile_open = false)}
 			class="fixed inset-0 z-50 bg-gray-700/20"
 		/>
 		<div
