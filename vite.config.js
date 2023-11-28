3c8ca711e45422682edad39e540cb4db1528982b
Logan <logan@fireflysoftware.dev>
2023-11-28 23:39:01 +0000

    Bugfix: Removed dependency from vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { imagetools } from '@zerodevx/svelte-img/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('vite').UserConfig} */
@@ -7,11 +6,6 @@ const config = {
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
-		imagetools({
-			// By default, directives are `?width=480;1024;1920&format=avif;webp;jpg`
-			defaultDirectives: () =>
-				new URLSearchParams('?width=240;360;480;1024;1920&format=avif;webp;jpg')
-		})
 	],
 	define: {
 		'process.env': process.env
