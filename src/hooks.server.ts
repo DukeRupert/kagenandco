12d6e2a6b69c417baeb584b1067379ccb1a39989
Logan Williams <logan@fireflysoftware.dev>
2023-12-22 18:16:31 +0000

    Installed Sentry

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://35148ba1588af6107b7acc14937c70bc@o4506440717893632.ingest.sentry.io/4506440719597568',
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		enhancedImages(),
-		sveltekit(),
-	],
-	define: {
+    plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "firefly-software",
+            project: "javascript-sveltekit"
+        }
+    }), enhancedImages(), sveltekit()],
+
+    define: {
 		'process.env': process.env
 	}
 };
