61675a9f87e27ef173e62850c1dcbf68d57ee54a
Logan <logan@fireflysoftware.dev>
2023-12-28 11:22:52 -0700

    Updated config to upload source maps to Sentry.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "firefly-software",
-            project: "javascript-sveltekit"
-        }
-    }), enhancedImages(), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'firefly-software',
+				project: 'kagenandco',
+				authToken: process.env.SENTRY_AUTH_TOKEN
+			}
+		}),
+		enhancedImages(),
+		sveltekit()
+	],
 
-    define: {
+	define: {
 		'process.env': process.env
 	}
 };
